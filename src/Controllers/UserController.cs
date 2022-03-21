4304a4db037338ef71a1de4f29b831aa023a7f6d
Juanma <jmfernandezc99@gmail.com>
2022-03-21 18:52:06 +0100

    Implement User Controller

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using JumboTravel.Api.src.Domain.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JumboTravel.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("GetUser")]
+        public async Task<IActionResult> GetUser([FromBody] GetUserRequest rq)
+        {
+            return Ok(rq);
+        }
+    }
+}
